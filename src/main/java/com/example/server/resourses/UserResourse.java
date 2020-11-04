fc82a2bff700d7cf59ebfd62e92c49dd59510ca5
Juliano Leocádio Batista <julianoleo@gmail.com>
2020-11-04 11:55:51 -0300

    IFTM - Spring Boot - aula 04/11/2020

---
+++ src/main/java/com/example/server/resourses/UserResourse.java
@@ -0,0 +1,19 @@
+package com.example.server.resourses;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.server.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResourse {
+		
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "maria", "maria@gmail.com", "9999-9999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
